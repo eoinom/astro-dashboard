8d95cdaef7af1ad18ef6d9ccec8d5257fafaff82
Eoin <eoinom@gmail.com>
2023-06-06 22:53:41 +0100

    Disable applying Tailwind base styles on each page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), vue(), svelte(), tailwind()]
+  integrations: [react(), vue(), svelte(), tailwind({
+    // Disable injecting a basic `base.css` import on every page.
+    config: { applyBaseStyles: false },
+  })]
 });
