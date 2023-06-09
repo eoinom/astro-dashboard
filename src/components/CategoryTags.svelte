b17bbbb62083e312984b16a8782b9c916563b809
Eoin <eoinom@gmail.com>
2023-06-09 23:47:17 +0100

    Add CategoryTags svelte component

---
+++ src/components/CategoryTags.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Chip, { Set, Text } from '@smui/chips';
+  import type { Product } from '../types/Product';
+
+  export let products: Product[];
+
+  let categories = products
+    .map((product) => product.category)
+    .filter((val, i, arr) => arr.indexOf(val) === i);
+
+  let selected = Array.from(categories);
+</script>
+
+<h1>Categories</h1>
+
+<Set chips={categories} let:chip filter bind:selected>
+  <Chip {chip} touch>
+    <Text>{chip}</Text>
+  </Chip>
+</Set>
+
